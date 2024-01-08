d171d5270f3ef6563e840a49be9a7a45576446e3
Bhupinder Singh <bs082045@gmail.com>
2024-01-08 17:05:17 -0500

    make home view and partials views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        
+        :root {
+            --bs-body-font-family: 'Inter', 'sans-serif';
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+
+    @include('partials/header')
+
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials/footer')
+
+
+</body>
+
+</html>
