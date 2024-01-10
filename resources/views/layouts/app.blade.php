59271032fb14836b9081caf735151b693835fac6
Bhupinder Singh <bs082045@gmail.com>
2024-01-09 21:48:05 -0500

    change font family and fix hero background path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,15 +15,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        
-        :root {
-            --bs-body-font-family: 'Inter', 'sans-serif';
-        }
-
-    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
 
 </head>
 
