95abbb09fddbeda32702c9461e77823762311b5f
Bhupinder Singh <bs082045@gmail.com>
2024-01-20 14:51:49 -0500

    fetch only loggedin user orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,7 +14,7 @@ class OrderController extends Controller
     public function __invoke(Request $request) 
     {
      
-        $orders = Order::with('orderItems.product')->simplePaginate();
+        $orders = Order::with('orderItems.product')->where('user_id', auth()->id())->simplePaginate();
 
         return view('orders.index', compact('orders'));
 
