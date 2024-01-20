da9e2b374787ecfd9d0bb45e90cefcdfac2fe55d
Bhupinder Singh <bs082045@gmail.com>
2024-01-20 18:43:14 -0500

    add app favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,16 @@
 
     <title>{{ config('app.name') }}</title>
 
-    <!-- Bootstrap -->
+    {{-- Bootstrap --}}
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
 
-    <!-- Fonts -->
+    {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/png">
 
 </head>
 
