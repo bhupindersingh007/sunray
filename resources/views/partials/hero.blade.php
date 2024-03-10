a477fbe345b73433d9b4650d733ea955eb4a4049
Bhupinder Singh <bs082045@gmail.com>
2024-03-10 19:19:03 -0400

    update banners

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -3,15 +3,15 @@ style="background: linear-gradient(42deg, rgba(0, 0, 0, 0.7), rgb(0 0 0 / 28%)),
 ">
     <div class="container text-white">
         <div class="d-lg-none">
-            <h1 class="mb-2 display-1 text-uppercase">Welcome to SUNRAY</h1>
+            <h1 class="mb-2 display-1 text-uppercase" style="font-size: 4rem;">Welcome to SUNRAY</h1>
             <p class="lead fs-3 mb-4">Explore from a Wide Range of Stylish Sunglasses & Eyeglasses.</p>
         </div>
 
         <div class="d-none d-lg-block">
             <h1 class="mb-2 display-2 text-uppercase">Welcome to SUNRAY</h1>
-            <p class="lead mb-4">Explore from a Wide Range of Stylish Sunglasses & Eyeglasses.</p>
+            <p class="fs-5 mb-4">Explore from a Wide Range of Stylish Sunglasses & Eyeglasses.</p>
         </div>
 
-        <a href="{{ route('products.index') }}" class="btn btn-primary text-uppercase py-2 px-3">Explore Products</a>
+        <a href="{{ route('products.index') }}" class="btn btn-primary text-uppercase py-2 px-3">Shop Products</a>
     </div>
 </section>
--- resources/views/partials/sale.blade.php
+++ resources/views/partials/sale.blade.php
@@ -1,7 +1,7 @@
-<section class="py-4" style="background: linear-gradient(42deg, rgba(0, 0, 0, 0.7), rgb(0 0 0 / 28%)), url('images/sale-banner.jpg') center / cover no-repeat">
+<section class="py-4 mb-4" style="background: linear-gradient(42deg, rgba(0, 0, 0, 0.7), rgb(0 0 0 / 28%)), url('images/sale-banner.jpg') center / cover no-repeat">
     <div class="col-md-10 col-lg-8 mx-md-auto p-5 text-white text-center">
         <h1 class="mb-3">25% OFF SALE</h1>
-        <p class="lead mb-4">"Shade your style with Sunray! Enjoy 25% off on a stunning selection of sunglasses and eyeglasses. Sale on now!"</p>
+        <p class="lead mb-4">Enjoy 25% off on a stunning selection of sunglasses and eyeglasses.</p>
         <a href="{{ route('products.index') }}?type=sale" class="btn btn-primary">Shop Now</a>
     </div>
 </section>
